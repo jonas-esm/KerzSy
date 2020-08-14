49ef452a3418d7073e68a82b9e25918dfa514907
jonas-esm <62783843+jonas-esm@users.noreply.github.com>
2020-08-15 01:00:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var express = require('express');
 var app = express();
 var mysql      = require('mysql');
@@ -20,25 +20,8 @@ app.use( bodyParser.urlencoded( {
 
 app.use( bodyParser.json() );
 
-app.use(cors())
-app.use(function (req, res, next) {
+//app.use(cors())
 
-   // Website you wish to allow to connect
-   res.setHeader('Access-Control-Allow-Origin', '*');
-
-   // Request methods you wish to allow
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-   // Request headers you wish to allow
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type' );
-
-   // Set to true if you need the website to include cookies in the requests sent
-   // to the API (e.g. in case you use sessions)
-   res.setHeader('Access-Control-Allow-Credentials', true);
-
-   // Pass to next layer of middleware
-   next();
-});
 app.use(function (req, res, next) {
 
    // Website you wish to allow to connect
