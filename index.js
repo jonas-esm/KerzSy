a222ad605d8ce72eca7049bd349a9afc7cc08041
jonas-esm <anas.esm@gmail.com>
2020-08-14 23:06:04 +0200

    from cloud

--- index.js
+++ index.js
@@ -603,7 +603,9 @@ app.use(cors())
 app.use(function (req, res, next) {
 
    // Website you wish to allow to connect
-   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Origin','*');
+   res.setHeader('Access-Control-Allow-Origin','https://kerzstore.com');
+
 
    // Request methods you wish to allow
    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
